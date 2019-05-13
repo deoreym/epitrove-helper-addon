17ee26096a0faab6fb8c001b111f5763ca94bebe
vaibhavghadge <ghadge.vaibhav09in5004@gmail.com>
2019-05-13 11:53:11 +0530

    Implemented UI for licesne management screen in wp dashboard

--- includes/views/view-license-page.php
+++ includes/views/view-license-page.php
@@ -33,27 +33,36 @@ if (isset($_POST)) {
     <h2>
         <?php echo __('Epitrove License Options', $this->pluginSlug); ?>
     </h2>
-
-    <form method="post" action="">
-        <p>
-            <label for="epi_registered_email">
-                <?php _e('Registered Email Address : ', 'epitrove-licensing') ?>
-            </label>
-            <input type="text" name="epi_registered_email" value="<?php echo esc_attr(static::getRegisteredEmail()); ?>" <?php echo $emailFieldReadOnly; ?>>
-            <input type="submit" class="button" name="epitrove-email-save" value="Save Email" />
-        </p>
-        <br>
-        <table class="epitrove-license-table">
-            <thead>
-                <th class="product-name-head"><?php _e('Product Name', 'epitrove-licensing'); ?></th>
-                <th class="license-key-head"><?php _e('License Key', 'epitrove-licensing'); ?></th>
-                <th class="license-status-head"><?php _e('License Status', 'epitrove-licensing'); ?></th>
-                <th class="actions-head"><?php _e('Actions', 'epitrove-licensing'); ?></th>
-            </thead>
-            <tbody>
-                <?php $this->showAllProductLicenses(); ?>
-                <?php do_action('epitrove_display_licensing_options'); ?>
-            </tbody>
-        </table>
-    </form>
+    <div class="wdm-admin-license-management-wrap">
+        <h2>License Management</h2>
+        <p class="steps-meta">Activate your product license in two steps</p>
+        <form method="post" action="">
+            <div class="wdm-home-page-banner-text-content-license">
+                <p class="legend">Step 1: Enter the Email ID used to register at Epitrove</p>
+                <div class="wdm-plugin-activation-field">
+                    <input type="text" name="epi_registered_email" value="<?php echo esc_attr(static::getRegisteredEmail()); ?>" <?php echo $emailFieldReadOnly; ?>>
+                    <?php if(!$emailFieldReadOnly) {?>
+                    <input type="submit" class="button button-primary" name="epitrove-email-save" value="Save Email" />
+                    <?php } else { ?>
+                    <p><i>Email address cannot be changed until all product licenses are deactivated</i></p>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="wdm-home-page-banner-text-content-license step-two">
+                <p class="legend">Step 2: Enter License Keys</p>
+                <table class="epitrove-license-table">
+                    <tbody>
+                        <tr>
+                            <td class="product-name-head"><?php _e('Product Name', 'epitrove-licensing'); ?></td>
+                            <td class="license-key-head"><?php _e('License Key', 'epitrove-licensing'); ?></td>
+                            <td class="license-status-head"><?php _e('License Status', 'epitrove-licensing'); ?></td>
+                            <td class="actions-head"><?php _e('Actions', 'epitrove-licensing'); ?></td>
+                        </tr>
+                        <?php $this->showAllProductLicenses(); ?>
+                        <?php do_action('epitrove_display_licensing_options'); ?>
+                    </tbody>
+                </table>
+            </div>
+        </form>
+    </div>
 </div>
